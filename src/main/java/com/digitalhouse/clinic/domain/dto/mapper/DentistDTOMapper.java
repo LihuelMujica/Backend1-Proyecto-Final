61aa77356539456968f63b1db8f050c73ec570fe
LihuelMujica <lihuelmujica@gmail.com>
2022-06-29 22:58:40 -0300

    Create and implement DentistDTOMapper

---
+++ src/main/java/com/digitalhouse/clinic/domain/dto/mapper/DentistDTOMapper.java
@@ -0,0 +1,8 @@
+package com.digitalhouse.clinic.domain.dto.mapper;
+
+import com.digitalhouse.clinic.domain.dto.DentistDTO;
+import com.digitalhouse.clinic.persistence.entity.Dentist;
+
+public interface DentistDTOMapper extends IDTOMapper<Dentist, DentistDTO> {
+
+}
---
+++ src/main/java/com/digitalhouse/clinic/domain/dto/mapper/impl/DentistDTOMapperImpl.java
@@ -0,0 +1,40 @@
+package com.digitalhouse.clinic.domain.dto.mapper.impl;
+
+import com.digitalhouse.clinic.domain.dto.DentistDTO;
+import com.digitalhouse.clinic.domain.dto.mapper.DentistDTOMapper;
+import com.digitalhouse.clinic.persistence.entity.Dentist;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class DentistDTOMapperImpl implements DentistDTOMapper {
+    private final ObjectMapper mapper;
+
+    public DentistDTOMapperImpl(ObjectMapper mapper) {
+        this.mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+    }
+
+
+    @Override
+    public DentistDTO toDTO(Dentist dentist) {
+        return mapper.convertValue(dentist, DentistDTO.class);    }
+
+    @Override
+    public List<DentistDTO> toDTO(List<Dentist> c) {
+        return c.stream().map(this::toDTO).collect(Collectors.toList());    }
+
+    @Override
+    public Dentist toEntity(DentistDTO dentistDTO) {
+        return mapper.convertValue(dentistDTO, Dentist.class);
+    }
+
+    @Override
+    public List<Dentist> toEntity(List<DentistDTO> d) {
+        return d.stream().map(this::toEntity).collect(Collectors.toList());
+    }
+}
