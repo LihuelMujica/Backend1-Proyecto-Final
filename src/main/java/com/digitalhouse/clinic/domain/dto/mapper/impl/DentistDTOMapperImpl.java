c7924df4cd7a648e7ff0de8d237bdfbdf0f9cf14
LihuelMujica <lihuelmujica@gmail.com>
2022-07-02 07:00:52 -0300

    Delete unused imports

--- src/main/java/com/digitalhouse/clinic/domain/dto/mapper/impl/DentistDTOMapperImpl.java
+++ src/main/java/com/digitalhouse/clinic/domain/dto/mapper/impl/DentistDTOMapperImpl.java
@@ -7,9 +7,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Component
 public class DentistDTOMapperImpl implements DentistDTOMapper {
     private final ObjectMapper mapper;
