f30e108b88cf502fd82cd1fd1a57090c53f47e1b
LihuelMujica <lihuelmujica@gmail.com>
2022-07-06 10:18:11 -0300

    Make getById throws ResourceNotFoundException

--- src/main/java/com/digitalhouse/clinic/domain/service/IPatientService.java
+++ src/main/java/com/digitalhouse/clinic/domain/service/IPatientService.java
@@ -1,13 +1,14 @@
 package com.digitalhouse.clinic.domain.service;
 
 import com.digitalhouse.clinic.domain.dto.PatientDTO;
+import com.digitalhouse.clinic.exception.ResourceNotFoundException;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface IPatientService {
     List<PatientDTO> getAll();
-    Optional<PatientDTO> getById(int id);
+    PatientDTO getById(int id) throws ResourceNotFoundException;
     PatientDTO create(PatientDTO patient);
     PatientDTO update(PatientDTO patient);
     boolean delete(int id);
--- src/main/java/com/digitalhouse/clinic/domain/service/impl/IPatientServiceImpl.java
+++ src/main/java/com/digitalhouse/clinic/domain/service/impl/IPatientServiceImpl.java
@@ -3,6 +3,7 @@ package com.digitalhouse.clinic.domain.service.impl;
 import com.digitalhouse.clinic.domain.dto.PatientDTO;
 import com.digitalhouse.clinic.domain.dto.mapper.PatientDTOMapper;
 import com.digitalhouse.clinic.domain.service.IPatientService;
+import com.digitalhouse.clinic.exception.ResourceNotFoundException;
 import com.digitalhouse.clinic.persistence.jparepository.PatientJPARepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -28,8 +29,9 @@ public class IPatientServiceImpl implements IPatientService {
     }
 
     @Override
-    public Optional<PatientDTO> getById(int id) {
-        return repository.findById(id).map(mapper::toDTO);
+    public PatientDTO getById(int id) throws ResourceNotFoundException {
+        return repository.findById(id).map(mapper::toDTO)
+                .orElseThrow(() -> new ResourceNotFoundException("Patient not found"));
     }
 
     @Override
