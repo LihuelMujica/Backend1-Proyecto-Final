f826c47de66ca68a9209a77d74a30bce6db8ac17
LihuelMujica <lihuelmujica@gmail.com>
2022-07-05 22:43:05 -0300

    Fix AppointmentDTOMapperImpl

--- src/main/java/com/digitalhouse/clinic/domain/dto/mapper/impl/AppointmentDTOMapperImpl.java
+++ src/main/java/com/digitalhouse/clinic/domain/dto/mapper/impl/AppointmentDTOMapperImpl.java
@@ -1,8 +1,12 @@
 package com.digitalhouse.clinic.domain.dto.mapper.impl;
 
 import com.digitalhouse.clinic.domain.dto.AppointmentDTO;
+import com.digitalhouse.clinic.domain.dto.DentistDTO;
+import com.digitalhouse.clinic.domain.dto.PatientDTO;
 import com.digitalhouse.clinic.domain.dto.mapper.AppointmentDTOMapper;
 import com.digitalhouse.clinic.persistence.entity.Appointment;
+import com.digitalhouse.clinic.persistence.entity.Dentist;
+import com.digitalhouse.clinic.persistence.entity.Patient;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
@@ -16,11 +20,18 @@ public class AppointmentDTOMapperImpl implements AppointmentDTOMapper {
 
     @Override
     public AppointmentDTO toDTO(Appointment appointment) {
-        return  mapper.convertValue(appointment,AppointmentDTO.class);
+
+        AppointmentDTO appointmentDTO = mapper.convertValue(appointment,AppointmentDTO.class);
+        appointmentDTO.setDentist(mapper.convertValue(appointment.getDentist(), DentistDTO.class));
+        appointmentDTO.setPatient(mapper.convertValue(appointment.getPatient(), PatientDTO.class));
+        return appointmentDTO;
     }
 
     @Override
     public Appointment toEntity(AppointmentDTO appointmentDTO) {
-        return mapper.convertValue(appointmentDTO,Appointment.class);
+        Appointment appointment = mapper.convertValue(appointmentDTO,Appointment.class);
+        appointment.setDentist(mapper.convertValue(appointmentDTO.getDentist(), Dentist.class));
+        appointment.setPatient(mapper.convertValue(appointmentDTO.getPatient(), Patient.class));
+        return appointment;
     }
 }
