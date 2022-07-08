5ddce96347ebcdad0cdd44cd9423cd91acff7363
LihuelMujica <lihuelmujica@gmail.com>
2022-07-08 20:07:21 -0300

    Add getByDni method

--- src/main/java/com/digitalhouse/clinic/domain/service/IPatientService.java
+++ src/main/java/com/digitalhouse/clinic/domain/service/IPatientService.java
@@ -1,6 +1,7 @@
 package com.digitalhouse.clinic.domain.service;
 
 import com.digitalhouse.clinic.domain.dto.PatientDTO;
+import com.digitalhouse.clinic.exception.ResourceAlreadyExistsException;
 import com.digitalhouse.clinic.exception.ResourceNotFoundException;
 
 import java.util.List;
@@ -8,7 +9,8 @@ import java.util.List;
 public interface IPatientService {
     List<PatientDTO> getAll();
     PatientDTO getById(int id) throws ResourceNotFoundException;
-    PatientDTO create(PatientDTO patient);
+    PatientDTO create(PatientDTO patient) throws ResourceAlreadyExistsException;
     PatientDTO update(PatientDTO patient) throws ResourceNotFoundException;
     void delete(int id) throws ResourceNotFoundException;
+    PatientDTO getByDni(String dni) throws ResourceNotFoundException;
 }
